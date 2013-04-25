3d8f1713a72e55196179ff7423638272e4d6fd84
Eugenio Vargas <eugenio.avargas@gmail.com>
2013-04-25 12:29:47 -0400

    New rails proyect

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Stickerfan::Application.config.secret_key_base = '8424a4b77b5ec37b9e2a659b67b7e63376ae93b12188a175ae7b91c9f0870eab1eea5d860c217c514e61b4ae70dc294daf924932bbe8773df0806960138b50f2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Stickerfan::Application.config.session_store :encrypted_cookie_store, key: '_stickerfan_session'
