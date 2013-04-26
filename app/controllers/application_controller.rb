c5124d8b854b14ac8d7fbea50d30d40229f5ce1d
Eugenio Vargas <eugenio.avargas@gmail.com>
2013-04-26 01:53:03 -0400

    Conect with API instagram and add Httparty

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  def index
+    
+    redirect_to "https://api.instagram.com/oauth/authorize/?client_id=22ea68b72f234ef9a3bf96720923c098&redirect_uri=http://localhost:2000/user&response_type=code"
+
+  end
+  
 end
---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,17 @@
+class UserController < ActionController::Base
+  protect_from_forgery with: :exception
+  
+  def index
+    info = { :body => {client_id: "22ea68b72f234ef9a3bf96720923c098",
+                      client_secret: "d8b170e6f84c4caf9ba83130d134ff6a",
+                      grant_type: "authorization_code",
+                      redirect_uri: "http://localhost:2000/user",
+                      code: params[:code] } 
+                    }
+                    
+    @response = HTTParty.post("https://api.instagram.com/oauth/access_token", info)
+    
+    @result = HTTParty.get("https://api.instagram.com/v1/users/#{@response["user"]["id"]}/media/recent/?access_token=#{@response["access_token"]}&count=-1")
+
+  end  
+end
